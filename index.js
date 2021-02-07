7ebd130fbe627b12f8412441e1b671a3ddbb75f6
Gherciu Gheorghe <gherciu553@gmail.com>
2021-02-07 17:51:48 +0200

    chore: publish

--- index.js
+++ index.js
@@ -1,18 +1,23 @@
 const fs = require('fs');
 const path = require('path');
 const {exec} = require('child_process');
+const colors = {
+	Black: '\u001B[30m',
+	Red: '\u001B[31m',
+	Green: '\u001B[32m',
+	Yellow: '\u001B[33m',
+	Blue: '\u001B[34m',
+	Magenta: '\u001B[35m',
+	Cyan: '\u001B[36m',
+	White: '\u001B[37m'
+};
 
 const every = command => {
-	const colors = {
-		Black: '\u001B[30m',
-		Red: '\u001B[31m',
-		Green: '\u001B[32m',
-		Yellow: '\u001B[33m',
-		Blue: '\u001B[34m',
-		Magenta: '\u001B[35m',
-		Cyan: '\u001B[36m',
-		White: '\u001B[37m'
-	};
+
+	if (!command.trim()) {
+		console.log(colors.Yellow, 'No CLI command provided!');
+		return
+	}
 
 	const folders = fs.readdirSync(process.cwd(), {withFileTypes: true})
 		.filter(dirent => dirent.isDirectory())
